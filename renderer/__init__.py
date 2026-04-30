419863a7973bf6e37426c57dcc0c8db6aae3808a
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-04-30 22:06:32 +0200

    Add init file for renderer

---
+++ renderer/__init__.py
@@ -0,0 +1,7 @@
+from .camera import Camera
+from .mesh import Mesh
+from .model import Model
+from .shader import Shader
+from .window import Window
+
+__all__ = ["Camera", "Mesh", "Model", "Shader", "Window"]
