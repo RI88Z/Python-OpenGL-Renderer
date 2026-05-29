a39037a84e703a72795601ebd8a10aff76d08819
Kacper Siemionek <siemionek.kacper22@gmail.com>
2026-05-30 00:25:58 +0200

    Add argparser

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import glfw
 import glm
 from OpenGL.GL import *
@@ -43,8 +45,74 @@ def process_input(window, delta_time):
         current_shader_type = "TOON"
 
 
+def parse_args():
+    parser = argparse.ArgumentParser(description="3D Model Viewer")
+
+    model = parser.add_argument_group("Model")
+    model.add_argument("--model", required=True, help="model file path")
+
+    texture = parser.add_argument_group("Textures")
+    texture.add_argument("--diffuse", required=True, help="diffuse texture path")
+    texture.add_argument("--specular", default=None, help="specular texture path")
+
+    light = parser.add_argument_group("Lights")
+    light.add_argument(
+        "--light",
+        choices=("none", "point", "directional", "spot"),
+        default="point",
+        help="light type",
+    )
+    light.add_argument(
+        "--light-pos",
+        nargs=3,
+        type=float,
+        metavar=("X", "Y", "Z"),
+        default=[1.2, 1.0, 2.0],
+        help="Pozycja światła (point/spot)",
+    )
+    # TODO: dodac opcje kierunku swiatla do directional
+    light.add_argument(
+        "--light-color",
+        nargs=3,
+        type=float,
+        default=[1.0, 1.0, 1.0],
+        help="light color",
+    )
+
+    cam_group = parser.add_argument_group("Camera")
+    cam_group.add_argument(
+        "--cam-pos",
+        nargs=3,
+        type=float,
+        default=[0.0, 0.0, 5.0],
+        metavar=("X", "Y", "Z"),
+        help="camera position",
+    )
+    cam_group.add_argument("--speed", type=float, default=2.5, help="camera speed")
+    cam_group.add_argument("--fov", type=float, default=45.0, help="field of view")
+    cam_group.add_argument("--far", type=float, default=1000.0, help="camera far arg")
+
+    s = parser.add_argument_group("Shaders")
+    s.add_argument(
+        "--shader",
+        choices=("PHONG", "GOURAUD", "TOON"),
+        default="PHONG",
+        help="shader type",
+    )
+
+    parser.add_argument("--rotate", action="store_true", help="rotate object")
+    parser.add_argument("--width", type=int, default=1280)
+    parser.add_argument("--height", type=int, default=720)
+
+    return parser.parse_args()
+
+
 def main():
-    app_window = Window(1280, 720, "3D Model Viewer - keys 1, 2, 3")
+    args = parse_args()
+
+    global current_shader_type
+    current_shader_type = args.shader
+    app_window = Window(args.width, args.height, "3D Model Viewer - keys 1, 2, 3")
     glfw.set_cursor_pos_callback(app_window.window, mouse_callback)
     glEnable(GL_DEPTH_TEST)
 
@@ -54,12 +122,17 @@ def main():
         "TOON": Shader("shaders/toon.vert", "shaders/toon.frag"),
     }
 
-    my_model = Model(
-        "assets/models/cube.obj",
-        diffuse_path="assets/textures/diffuse.jpg",
-    )
+    camera.position = glm.vec3(*args.cam_pos)
+    camera.movement_speed = args.speed
+    camera.fov = args.fov
+    camera.far = args.far
+    camera.update_camera_vectors()
 
-    light_color = glm.vec3(1.0, 1.0, 1.0)
+    my_model = Model(args.model, diffuse_path=args.diffuse, specular_path=args.specular)
+
+    # TODO: pendziwiatr dodaj swiatla okok??
+    light_pos = glm.vec3(*args.light_pos)
+    light_color = glm.vec3(*args.light_color)
 
     last_frame = 0.0
 
@@ -82,16 +155,14 @@ def main():
         projection = camera.get_projection_matrix(app_window.width, app_window.height)
         view = camera.get_view_matrix()
         model_mat = glm.mat4(1.0)
-        model_mat = glm.rotate(
-            model_mat, float(glfw.get_time()) * 0.5, glm.vec3(0.0, 1.0, 0.0)
-        )
+        if args.rotate:
+            model_mat = glm.rotate(
+                model_mat, float(glfw.get_time()) * 0.5, glm.vec3(0.0, 1.0, 0.0)
+            )
 
         active_shader.set_mat4("projection", projection)
         active_shader.set_mat4("view", view)
         active_shader.set_mat4("model", model_mat)
-
-        t = glfw.get_time()
-        light_pos = glm.vec3(2.0 * glm.sin(t), 1.0, 2.0 * glm.cos(t))
         active_shader.set_vec3("lightPos", light_pos)
         active_shader.set_vec3("lightColor", light_color)
         active_shader.set_vec3("viewPos", camera.position)
